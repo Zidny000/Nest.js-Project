29d884537d54f0830e42091573f84d0b59524cd8
Zidny000 <t.m.zidny@gmail.com>
2025-10-04 04:22:28 +0600

    auth.module configuration with jwt and passport

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,17 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { User } from './entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({}),
+    PassportModule
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService], //jwt strategy, roles guard -> todo
+  exports: [AuthService] //roles guard --> todo
 })
 export class AuthModule {}
