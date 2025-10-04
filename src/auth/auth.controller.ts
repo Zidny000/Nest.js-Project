dc201f1382ad24c09568913288cc0ba0e5cf36d3
Zidny000 <t.m.zidny@gmail.com>
2025-10-05 01:30:11 +0600

    auth controller updated with authentication and authorization [/profile, /create-admin]

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,13 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Get, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
+import { CurrentUser } from './decorators/current-user.decorator';
+import { UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { RolesGuard } from './guards/roles-guard';
+import { UserRole } from './entities/user.entity';
+import { Roles } from './decorators/roles.decorators';
 
 @Controller('auth')
 export class AuthController {
@@ -18,8 +24,21 @@ export class AuthController {
   }
 
   @Post('refresh-token')
-  async refreshToken(@Body() refreshTokenDto: string) {
+  async refreshToken(@Body('refreshToken') refreshTokenDto: string) {
     return this.authService.refreshToken(refreshTokenDto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@CurrentUser() user: any) {
+    return user;
+  }
+
+  @Post('create-admin')
+  @Roles(UserRole.ADMIN)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  createAdmin(@Body() registerDto : RegisterDto){
+    return this.authService.createAdmin(registerDto)
   }
 
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,6 +5,8 @@ import { User } from './entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { RolesGuard } from './guards/roles-guard';
 
 @Module({
   imports: [
@@ -13,7 +15,7 @@ import { PassportModule } from '@nestjs/passport';
     PassportModule
   ],
   controllers: [AuthController],
-  providers: [AuthService], //jwt strategy, roles guard -> todo
-  exports: [AuthService] //roles guard --> todo
+  providers: [AuthService, JwtStrategy, RolesGuard],
+  exports: [AuthService, RolesGuard] //roles guard --> todo
 })
 export class AuthModule {}
